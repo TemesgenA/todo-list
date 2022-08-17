de1f4b27bca4bda774e9601463bd47a1a20953f5
TemesgeA <temesgen.ayneka@gmail.com>
2022-08-17 11:16:47 +0300

    change the default branch

--- modules/newTask.js
+++
@@ -1,8 +0,0 @@
-import { toDoListData } from './todo.js';
-import { setData } from './setItems.js';
-
-export default function addTask(taskinput) {
-  const taskList = toDoListData();
-  taskList.push(taskinput);
-  setData(taskList);
-}
--- modules/todo.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export const toDoListData = () => JSON.parse(window.localStorage.getItem('taskData') || '[]');
